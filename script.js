b4037cc262ec9fc3efdcc7ad75126ec0aaa4cef3
Bibek <chakrabortybibek408@gmail.com>
2025-11-08 12:20:58 +0530

    Added input field

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('search-btn').addEventListener('click', () => {
+  const city = document.getElementById('city-input').value;
+  console.log('City entered:', city);
+
+  // Later this city value will be used to fetch weather data from API
+
+});
